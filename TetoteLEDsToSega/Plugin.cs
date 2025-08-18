1d67a1eeab1ca5f2a8308743c2fd847593e4d5cd
kyoubate-haruka <46010460+kyoubate-haruka@users.noreply.github.com>
2025-08-18 14:19:17 +0200

    Misc fixes, disallow duplicate sends

--- TetoteLEDsToSega/Plugin.cs
+++ TetoteLEDsToSega/Plugin.cs
@@ -1,4 +1,5 @@
 ﻿using System;
+using System.Collections;
 using System.Linq;
 using BepInEx;
 using BepInEx.Configuration;
@@ -18,14 +19,19 @@ public class Plugin : BaseUnityPlugin {
     public static LED_837_15093_06 Leds;
 
     private ConfigEntry<int> configLedPort;
+    private ConfigEntry<bool> configSegaLibLog;
 
     private void Awake() {
         // Plugin startup logic
         Logger = base.Logger;
 
         configLedPort = Config.Bind("General", "LED COM Port", 9, "COM Port on which the LED board is connected to.");
+        configSegaLibLog = Config.Bind("Debugging", "SegaLib Logging", false, "Enables Sega835Lib debug logs");
 
-        Log.LogMessageWritten += LogOnLogMessageWritten;
+        if (configSegaLibLog.Value) {
+            Haruka.Arcade.SEGA835Lib.Debugging.Log.Mute = true;
+        }
+
         Leds = new LED_837_15093_06(configLedPort.Value);
         DeviceStatus ret = Leds.Connect();
         if (ret != DeviceStatus.OK) {
@@ -55,41 +61,39 @@ public class Plugin : BaseUnityPlugin {
         }
     }
 
-    private static void LogOnLogMessageWritten(LogEntry obj) {
-        if (obj.Color == ConsoleColor.Yellow) {
-            Logger.LogWarning("SegaLib: " + obj.Message);
-        } else if (obj.Color == ConsoleColor.Red) {
-            Logger.LogError("SegaLib: " + obj.Message);
-        } else {
-            Logger.LogInfo("SegaLib: " + obj.Message);
-        }
-    }
-
-    private static void UpdateLeds(byte[] buf) {
+    private static Color[] prev_leds = new Color[18];
+    private static void UpdateLeds(JVS_IO.Output.LEDAnalog rd, JVS_IO.Output.LEDAnalog rc, JVS_IO.Output.LEDAnalog ru, JVS_IO.Output.LEDAnalog lu, JVS_IO.Output.LEDAnalog lc, JVS_IO.Output.LEDAnalog ld) {
         if (Leds == null) {
             return;
         }
 
         Color[] leds = Enumerable
-            .Repeat(Color.FromArgb(buf[0], buf[1], buf[2]), 11) // left upper
-            .Concat(Enumerable.Repeat(Color.FromArgb(buf[3], buf[4], buf[5]), 11)) // left center
-            .Concat(Enumerable.Repeat(Color.FromArgb(buf[6], buf[7], buf[8]), 11)) // left bottom
-            .Concat(Enumerable.Repeat(Color.FromArgb(buf[12], buf[13], buf[17]), 11)) // right upper
-            .Concat(Enumerable.Repeat(Color.FromArgb(buf[18], buf[19], buf[20]), 11)) // right center
-            .Concat(Enumerable.Repeat(Color.FromArgb(buf[21], buf[22], buf[23]), 11)) // right bottom
+            .Repeat(Color.FromArgb(rd.Red, rd.Green, rd.Blue), 11) // right bottom
+            .Concat(Enumerable.Repeat(Color.FromArgb(rc.Red, rc.Green, rc.Blue), 11)) // right center
+            .Concat(Enumerable.Repeat(Color.FromArgb(ru.Red, ru.Green, ru.Blue), 11)) // right upper
+            .Concat(Enumerable.Repeat(Color.FromArgb(lu.Red, lu.Green, lu.Blue), 11)) // left upper
+            .Concat(Enumerable.Repeat(Color.FromArgb(lc.Red, lc.Green, lc.Blue), 11)) // left center
+            .Concat(Enumerable.Repeat(Color.FromArgb(ld.Red, ld.Green, ld.Blue), 11)) // left bottom
             .ToArray();
+
+        if (StructuralComparisons.StructuralEqualityComparer.Equals(prev_leds, leds)) {
+            return;
+        }
 
         DeviceStatus ret = Leds.SetLEDs(leds);
         if (ret != DeviceStatus.OK) {
             Logger.LogError("Failed to set LEDs! " + ret);
             Leds = null;
         }
+
+        prev_leds = leds;
     }
 
     public class Patches {
         [HarmonyPostfix, HarmonyPatch(typeof(JVS_IO), "Update")]
         static void Update(JVS_IO __instance) {
-            Plugin.UpdateLeds(__instance.buf.OutPWM);
+            UpdateLeds(__instance.output.TapeLED_R_DOWN, __instance.output.TapeLED_R_CENTER, __instance.output.TapeLED_R_UP, __instance.output.TapeLED_L_UP, __instance.output.TapeLED_L_CENTER, __instance.output.TapeLED_L_DOWN);
         }
+
     }
 }
